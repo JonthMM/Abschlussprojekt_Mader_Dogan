a48025876e046994bbd395a19cca26a835330050
Jonathan Mader <96208636+JonthMM@users.noreply.github.com>
2022-09-14 22:37:57 +0200

    Update start.js

--- routes/start.js
+++ routes/start.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * GET Befehl für die Homepage
+ */
 router.get("/", function (req, res, next) {
   res.render("start", { title: "Startseite" });
 });
